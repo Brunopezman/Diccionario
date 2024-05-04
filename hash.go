613f542bf7ef59e55d4834aec8bba569188c658c
brunopez <bpezman@fi.uba.ar>
2024-05-04 19:12:25 -0300

    .

--- hash.go
+++ hash.go
@@ -6,13 +6,17 @@ import (
 )
 
 const (
-	_CAPACIDAD_INICIAL int = 10
+	_TAM int = 10
 )
 
+type parClaveValor[K comparable, V any] struct {
+	clave K
+	valor V
+}
+
 type hash[K comparable, V any] struct {
-	datos    []TDALista.Lista[K]
-	clave    K
-	valor    V
+	tabla    []TDALista.Lista[parClaveValor[K, V]]
+	tam      int
 	cantidad int
 }
 
@@ -22,8 +26,16 @@ type iterDiccionario[K comparable, V any] struct {
 	diccionario *hash[K, V]
 }
 
+func CrearParClaveValor[K comparable, V any](clave K, valor V) *parClaveValor[K, V] {
+	return &parClaveValor[K, V]{clave: clave, valor: valor}
+}
+
 func CrearHash[K comparable, V any]() Diccionario[K, V] {
-	return &hash[K, V]{datos: make([]TDALista.Lista[K], _CAPACIDAD_INICIAL), cantidad: 0}
+	return &hash[K, V]{
+		tabla:    make([]TDALista.Lista[parClaveValor[K, V]], _TAM),
+		tam:      _TAM,
+		cantidad: 0,
+	}
 }
 
 func convertirABytes[K comparable](clave K) []byte {
@@ -39,12 +51,12 @@ func fhash[K comparable](clave K, capacidad int) int {
 }
 
 func (dic *hash[K, V]) Guardar(clave K, dato V) {
-	pos := fhash(clave, _CAPACIDAD_INICIAL)
-	if dic.datos[pos].EstaVacia() {
-		dic.datos[pos].InsertarPrimero(clave)
+	pos := fhash(clave, _TAM)
+	if dic.tabla[pos].EstaVacia() {
+		dic.tabla[pos].InsertarUltimo(*CrearParClaveValor(clave, dato))
 	}
-	for iter := dic.datos[pos].Iterador(); iter.HaySiguiente(); iter.Siguiente() {
-		iter.Insertar(clave)
+	for iter := dic.tabla[pos].Iterador(); iter.HaySiguiente(); iter.Siguiente() {
+		iter.Insertar(*CrearParClaveValor(clave, dato))
 	}
 
 	dic.cantidad++
@@ -52,10 +64,10 @@ func (dic *hash[K, V]) Guardar(clave K, dato V) {
 }
 
 func (dic *hash[K, V]) Pertenece(clave K) bool {
-	pos := fhash(clave, _CAPACIDAD_INICIAL)
-	for iter := dic.datos[pos].Iterador(); iter.HaySiguiente(); iter.Siguiente() {
-		elemento := iter.VerActual()
-		if elemento == clave {
+	pos := fhash(clave, _TAM)
+	for iter := dic.tabla[pos].Iterador(); iter.HaySiguiente(); iter.Siguiente() {
+		parClaveValor := iter.VerActual()
+		if parClaveValor.clave == clave {
 			return true
 		}
 	}
@@ -64,8 +76,8 @@ func (dic *hash[K, V]) Pertenece(clave K) bool {
 }
 
 func (dic *hash[K, V]) Obtener(clave K) V {
-	pos := fhash(clave, _CAPACIDAD_INICIAL)
-	for iter := dic.datos[pos].Iterador(); iter.HaySiguiente(); iter.Siguiente() {
+	pos := fhash(clave, _TAM)
+	for iter := dic.tabla[pos].Iterador(); iter.HaySiguiente(); iter.Siguiente() {
 		elemento := iter.VerActual()
 		if elemento == clave {
 			return dic.valor
@@ -75,8 +87,8 @@ func (dic *hash[K, V]) Obtener(clave K) V {
 }
 
 func (dic *hash[K, V]) Borrar(clave K) V {
-	pos := fhash(clave, _CAPACIDAD_INICIAL)
-	for iter := dic.datos[pos].Iterador(); iter.HaySiguiente(); iter.Siguiente() {
+	pos := fhash(clave, _TAM)
+	for iter := dic.tabla[pos].Iterador(); iter.HaySiguiente(); iter.Siguiente() {
 		elemento, valor := dic.Iterador().VerActual()
 		if elemento == clave {
 			iter.Borrar()
@@ -100,7 +112,7 @@ func (dic *hash[K, V]) Iterador() IterDiccionario[K, V] {
 }
 
 func (iter *iterDiccionario[K, V]) HaySiguiente() bool {
-	return iter.diccionario.datos != nil
+	return iter.diccionario.tabla != nil
 }
 
 func (iter *iterDiccionario[K, V]) VerActual() (K, V) {
